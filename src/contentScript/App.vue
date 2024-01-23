c7747133ba8b49e0b1a7f9b4e141f98a7b08ded9
Johannes Idarsson <johannes.idarsson@gmail.com>
2024-01-23 11:38:02 +0100

    created ChatWindow component (without any real content)

--- src/contentScript/App.vue
+++ src/contentScript/App.vue
@@ -1,42 +1,17 @@
 <template>
-    <div v-if="showDrawer" class="extension-drawer">
-        <HelloWorld
-            message="Welcome To Your New Vue.js + TypeScript Chrome Extension!"
-        />
-    </div>
+    <ChatWindow v-if="showChat" />
 
-    <ExtensionToggle v-model="showDrawer" />
+    <ExtensionToggle v-model="showChat" />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import ChatWindow from "./components/ChatWindow.vue";
 import ExtensionToggle from "./components/ExtensionToggle.vue";
 
-const showDrawer = ref(false);
+const showChat = ref(false);
 </script>
 
 <style scoped>
-.extension-drawer {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 20px;
-    position: fixed;
-    bottom: 90px;
-    right: 20px;
-    width: 300px;
-    height: 400px;
-    background-color: #fff;
-    border-radius: 5px;
-    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
-    z-index: 1000000000000000000000000000000000000000000000000000000000000000000000000;
-}
+/* TODO: Add inter? */
 </style>
---
+++ src/contentScript/components/ChatWindow.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="kwery-chat-window">
+        <HelloWorld
+            message="Welcome To Your New Vue.js + TypeScript Chrome Extension!"
+        />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from "../../components/HelloWorld.vue";
+</script>
+
+<style>
+.kwery-chat-window {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 20px;
+    position: fixed;
+    bottom: 90px;
+    right: 20px;
+    width: 300px;
+    height: 400px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+    z-index: 1000000000000000000000000000000000000000000000000000000000000000000000000;
+}
+</style>
