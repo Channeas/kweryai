3545095a82288a47110585eabb707ec8bd67f181
Johannes Idarsson <johannes.idarsson@gmail.com>
2024-02-27 17:25:05 +0100

    moved getApiKey above setApiKey for consistency

--- src/worker/storage/apiKey.ts
+++ src/worker/storage/apiKey.ts
@@ -1,11 +1,11 @@
-export async function setApiKey(apiKey: string) {
-    await chrome.storage.local.set({
-        kweryOpenaiApiKey: apiKey
-    });
-}
-
 export async function getApiKey(): Promise<string> {
     const { kweryOpenaiApiKey: apiKey } =
         await chrome.storage.local.get("kweryOpenaiApiKey");
     return apiKey;
+}
+
+export async function setApiKey(apiKey: string) {
+    await chrome.storage.local.set({
+        kweryOpenaiApiKey: apiKey
+    });
 }
