f7161fccf47098287e7ca8270b9c3e4870675f0a
Johannes Idarsson <johannes.idarsson@gmail.com>
2024-03-01 14:33:59 +0100

    broke out the openai system prompt to a prompts file

--- src/worker/aiProviders/openai.ts
+++ src/worker/aiProviders/openai.ts
@@ -5,6 +5,8 @@ import { getApiKey } from "@/worker/storage/apiKey";
 import { getSelectedModel } from "../storage/selectedModel";
 import getErrorAsString from "@/utils/getErrorAsString";
 
+import { openaiSystemPrompt } from "@/prompts";
+
 const providerKey = "openai";
 
 export class OpenaiProvider implements Provider {
@@ -102,7 +104,7 @@ export class OpenaiProvider implements Provider {
 
         // TODO: Possibly move to a config file
         const systemMessage = {
-            content: `You are a question and answer system. Your job is to succinctly and accurately answer questions based on a context that is given to you. Do not use facts or knowledge that is not part of the given context. If the answer is not in the context, respond with "I cannot tell based on the text on this page"\nDo not make assumptions, everything in your answer has to be explicitly stated in the context.\n\nAnswer the most recent user message. Unless the message says otherwise, respond in at most 3 sentences.\n\nContext: "${context}"`,
+            content: openaiSystemPrompt(context),
             role: "system"
         };
 
