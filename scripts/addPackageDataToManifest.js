6bb914e1af998533643aa839e2a69a7b46c1a95e
Johannes Idarsson <johannes.idarsson@gmail.com>
2024-04-27 18:01:29 +0200

    switched the name to have upper case letters

--- scripts/addPackageDataToManifest.js
+++ scripts/addPackageDataToManifest.js
@@ -3,17 +3,17 @@ const package_json = require("../package.json");
 module.exports = function addPackageDataToManifest(manifestAsBuffer) {
     const originalManifest = JSON.parse(manifestAsBuffer.toString());
 
-    let name = package_json.name;
+    let name = originalManifest.name || package_json.name;
     if (process.env.NODE_ENV === "development") {
-        name = `DEV: ${package_json.name}`;
+        name = `DEV: ${name}`;
     }
 
     // Fill some properties of the extension manifest using package.json data
     const modifiedManifest = {
-        name,
         version: package_json.version,
         description: package_json.description,
-        ...originalManifest
+        ...originalManifest,
+        name
     };
 
     return JSON.stringify(modifiedManifest, null, 4);
