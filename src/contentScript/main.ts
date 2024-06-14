d130bffaff19bb390ab1e3ef4c5d80e4c84cc019
Johannes Idarsson <johannes.idarsson@gmail.com>
2024-06-14 22:29:42 +0200

    removed createExtensionContainerAndRoot as a util since it's only used in one file - the content script (essentially reversing 2804a09598fafbd70eccc8606351f156c5404305)

--- src/contentScript/main.ts
+++ src/contentScript/main.ts
@@ -1,8 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import createExtensionContainerAndRoot from "@/utils/createExtensionContainerAndRoot";
 
-const { extensionContainer, extensionRoot } = createExtensionContainerAndRoot();
+const extensionRoot = document.createElement("div");
+extensionRoot.setAttribute("id", "kwery-extension-root");
+const shadow = extensionRoot.attachShadow({ mode: "open" });
+
+const extensionContainer = document.createElement("div");
+extensionContainer.setAttribute("id", "kwery-extension-container");
+shadow.appendChild(extensionContainer);
+
+const styleSheet = document.createElement("link");
+styleSheet.setAttribute("rel", "stylesheet");
+styleSheet.setAttribute("href", chrome.runtime.getURL("css/script.css"));
+shadow.appendChild(styleSheet);
 
 document.body.appendChild(extensionRoot);
 
